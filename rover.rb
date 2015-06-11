023709471ace709b2e0b0b3b80175980874c3c6a
Michael Fich <michaeltf@gmail.com>
2015-06-11 15:39:28 -0400

    Exercise 2: Refined the method for the direction of the Rover class by putting it into an array and the direction is set to the first element of the array.  Also added the 'turn' class to change the direction of the rover left or right.

--- rover.rb
+++ rover.rb
@@ -1,23 +1,52 @@
 # Rover
 class Rover
-  attr_accessor :x, :y, :direction
-
   def initialize(input)
     input = input.split
 
     @x = input[0]
     @y = input[1]
+    @direction = ['N', 'E', 'S', 'W']
 
-    @direction = input[2]
+    direction = input[2]
+
+    if direction == 'E'
+      @direction.rotate!
+    elsif direction == 'S'
+      @direction.rotate!(2)
+    elsif direction == 'W'
+      @direction.rotate!(-1)
+    end
+  end
+
+  def read_instruction(instruction)
+    instruction = instruction.chars
+  end
+
+  def turn(direction)
+    if direction == 'R'
+      @direction.rotate
+    elsif direction == 'L'
+      @direction.rotate!(-1)
+    end
   end
 
   def to_s
-    "Position: (#{@x}, #{@y})\nDirection: #{@direction}"
+    "Position: (#{@x}, #{@y})\nDirection: #{@direction[0]}"
   end
 end
 
-puts "Enter the first line of input:"
-input = gets.chomp
+input1 = "1 2 W"
+instructions1 = "LMLMLMLMM"
 
-rover = Rover.new(input)
-puts rover
+puts "Enter the first line of input:"
+print ">> "
+puts input1
+# input1 = gets.chomp
+
+puts "Enter the second line of input:"
+print ">> "
+puts instructions1
+# instructions1 = gets.chomp
+
+rover = Rover.new(input1)
+rover.read_instruction(instructions1)
