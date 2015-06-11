90307bc8202f62990dafeafc2e7ffdb7b198625f
Michael Fich <michaeltf@gmail.com>
2015-06-11 13:30:38 -0400

    Exercise 1: Created the parent class 'Person' from which 'Student' and 'Instructor' now inherit from.  Set an initalize method to accept a parameter 'name' which is passed to the parent from each child class.

--- people.rb
+++ people.rb
@@ -1,10 +1,24 @@
-class Student
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
+
+class Student < Person
+	def initialize(name)
+		super(name)
+	end
+
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < Person
+	def initialize(name)
+		super(name)
+	end
+
 	def teach
 		puts "Everything in Ruby is an Object."
 	end
